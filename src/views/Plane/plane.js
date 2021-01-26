dc61c096070d06eaabc408cef6edbb074ed8cd05
5312 <1765080897@qq.com>
2021-01-26 19:41:04 +0800

    侧边

--- src/views/Plane/plane.js
+++ src/views/Plane/plane.js
@@ -18,10 +18,13 @@ class Line {
 
     this.groupOne();
 
-    this.helper()
+    this.helper();
+
   }
   bottom() {
-    var geometry = new THREE.PlaneBufferGeometry(1000, 100)
+    let height = 100,
+      width = 1000
+    var geometry = new THREE.PlaneBufferGeometry(width, height)
 
     var material = new THREE.MeshBasicMaterial({
       color: '#000',
@@ -31,6 +34,42 @@ class Line {
     // Create the final object to add to the scene
     var curveObject = new THREE.Mesh(geometry, material);
     this.group.add(curveObject)
+
+    // 两侧
+    /**--------------------------------------------- */
+    var material1 = new THREE.MeshBasicMaterial({
+      color: '#0546BE',
+      side: THREE.DoubleSide
+    });
+    var left = new THREE.Mesh(geometry, material1);
+    left.name = 'left'
+    left.position.set(0, -50, 50)
+    left.rotation.set(Math.PI / 2, 0, 0)
+    this.group.add(left)
+    /**--------------------------------------------- */
+    var material1 = new THREE.MeshBasicMaterial({
+      color: '#0546BE',
+      side: THREE.DoubleSide
+    });
+    var right = new THREE.Mesh(geometry, material1);
+    right.name = 'right'
+    right.position.set(0, 50, 50)
+    right.rotation.set(Math.PI / 2, 0, 0)
+    this.group.add(right)
+    /**--top shape------------------------------------------- */
+    var geometry2 = new THREE.CylinderBufferGeometry(height / 2, height / 2, width, 3, 3, true, 0, Math.PI);
+    var material2 = new THREE.MeshBasicMaterial({
+      transparent: true,//开启透明度
+      opacity: 0.5,//设置透明度具体值
+      color: '#0546BE',//三角面颜色
+      side: THREE.DoubleSide//两面可见
+    });//材质对象
+    var top = new THREE.Mesh(geometry2, material2);//网格模型对象
+    top.name = 'top'
+    top.position.set(0, 0, 100)
+    top.rotation.set(Math.PI / 2, 0, Math.PI / 2)
+    /**--------------------------------------------- */
+    this.group.add(top)
   }
   allGroup() {
     this.group = new THREE.Group();
@@ -39,8 +78,10 @@ class Line {
     this.scene.add(this.group)
   }
   groupOne() {
+    let height = 100,
+      width = 1000
     // 把外层对象的中心移动到，旋转中心，然会把目标移动会原点，然后旋转外层对象
-    var geometry = new THREE.PlaneBufferGeometry(1000, 100)
+    var geometry = new THREE.PlaneBufferGeometry(width, height)
 
     var material = new THREE.MeshBasicMaterial({
       color: '#000',
@@ -55,7 +96,43 @@ class Line {
     group.add(curveObject); // 把'门'添加进外层对象中
     curveObject.position.set(-500, 0, 0); // 调整门在外层对象中的相对位置
     group.rotation.set(0, Math.PI / 4, 0)
-
+    // 两侧
+    /**-------------------left-------------------------- */
+    var material1 = new THREE.MeshBasicMaterial({
+      color: '#0546BE',
+      side: THREE.DoubleSide
+    });
+    var left = new THREE.Mesh(geometry, material1);
+    left.name = 'left'
+    left.position.set(-500, -50, 50)
+    left.rotation.set(Math.PI / 2, 0, 0)
+    group.add(left)
+    /**--------------------------------------------- */
+    /**---------------------right------------------------ */
+    var material1 = new THREE.MeshBasicMaterial({
+      color: '#0546BE',
+      side: THREE.DoubleSide
+    });
+    var left = new THREE.Mesh(geometry, material1);
+    left.name = 'right'
+    left.position.set(-500, 50, 50)
+    left.rotation.set(Math.PI / 2, 0, 0)
+    group.add(left)
+    /**--------------------------------------------- */
+    /**--top shape------------------------------------------- */
+    var geometry2 = new THREE.CylinderBufferGeometry(height / 2, height / 2, width, 3, 3, true, 0, Math.PI);
+    var material2 = new THREE.MeshBasicMaterial({
+      transparent: true,//开启透明度
+      opacity: 0.5,//设置透明度具体值
+      color: '#0546BE',//三角面颜色
+      side: THREE.DoubleSide//两面可见
+    });//材质对象
+    var top = new THREE.Mesh(geometry2, material2);//网格模型对象
+    top.name = 'top'
+    top.position.set(-500, 0, 100)
+    top.rotation.set(Math.PI / 2, 0, Math.PI / 2)
+    group.add(top)
+    /**--------------------------------------------- */
     this.group.add(group)
     this.groupTwo(group);
   }
@@ -63,21 +140,32 @@ class Line {
 
   }
   groupTwo(groupOne) {
-    const geometry1 = new THREE.PlaneBufferGeometry(100, 400)
+    const geometry = new THREE.PlaneBufferGeometry(100, 400)
     const material = new THREE.MeshBasicMaterial({
       color: '#000',
       side: THREE.DoubleSide
     });
-    const mesh = new THREE.Mesh(geometry1, material);
-
+    const mesh = new THREE.Mesh(geometry, material);
+    /** ----------------------------------------------- */
     let group2 = new THREE.Group();
     group2.name = 'group2'
     group2.position.set(-1000, 0, 0)
     group2.add(mesh)
     mesh.position.set(-50, -150, 0)
-
     group2.rotation.set(0, -Math.PI / 4, 0)
-
+    /** ----------------------------------------------- */
+    /**-------------------left-------------------------- */
+    var material1 = new THREE.MeshBasicMaterial({
+      color: '#0546BE',
+      side: THREE.DoubleSide
+    });
+    var left = new THREE.Mesh(geometry, material1);
+    left.name = 'left'
+    left.position.set(0, -150, 0)
+    left.rotation.set(Math.PI / 2, 0, 0)
+    group2.add(left)
+    /**--------------------------------------------- */
+    // 加入组
     groupOne.add(group2)
   }
   // 辅助元素
